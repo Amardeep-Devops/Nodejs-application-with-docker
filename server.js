6ec9b0d53248adbb40f89ef06a573d94ec193e8a
Amardeep-Devops <63974109+Amardeep-Devops@users.noreply.github.com>
2020-06-12 22:34:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,7 @@
-'use strict';
 
-const express = require('express');
-
-// Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
-
-// App
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World');
+var express = require('express');
+var app = express();
+app.use(express static ('public'));
 });
 
 app.listen(PORT, HOST);
